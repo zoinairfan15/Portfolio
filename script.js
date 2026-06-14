46549277c88e8f081cecb55fe72d8c9cd83a2f23
zoinairfan15 <zoinairfan008@gmail.com>
2026-06-14 12:56:02 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll for all nav links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
